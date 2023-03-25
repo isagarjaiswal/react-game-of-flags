1590bb9849306401ba9d901cefaf113aa14673c6
isagarjaiswal <sagarjaiswal815555@gmail.com>
2023-03-25 20:04:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import nations from "./nations";
+import "flag-icon-css/css/flag-icons.css";
+import "./App.css";
+
+const App = () => {
+  const [country, setCountry] = useState([]);
+  const [flagCountry, setFlagCountry] = useState({});
+  const [score,setScore]=useState({total:0,correct:0,incorrect:0});
+  const [showAnswer,setShowAnswer]=useState(false)
+  const[selected,setSelected] = useState({})
+
+  const generateRandomNations = () => {
+    let ct = [];
+
+    for (let i = 0; i < 4; i++) {
+      const r = Math.floor(Math.random() * nations.length);
+      ct.push(nations[r]);
+    }
+    setCountry(ct);
+    const index = Math.floor(Math.random()*4)
+    setFlagCountry(ct[index])
+  }
+
+  const checkAnswer = (country)=>{
+     setSelected(country)
+    if(country.name===flagCountry.name){
+      setScore({...score, correct:score.correct+1,total:score.total+1})
+    }else{
+      setScore({...score,incorrect:score.incorrect+1,total:score.total+1})
+    }
+    setShowAnswer(true)
+  setTimeout(()=>{
+  setShowAnswer(false)
+  nextQuestion();
+   },4000)
+  }
+
+  const nextQuestion =()=>{
+    generateRandomNations()
+  }
+  useEffect(() => {
+    generateRandomNations();
+  }, []);
+
+  return (
+    <div className="App">
+      <div>total {score.total}  correct {score.correct} incorrect {score.incorrect}</div>
+      { flagCountry.name ? <span className={`flag-icon flag-icon-${flagCountry['alpha-2'].toLowerCase()}`}></span>
+: null
+      }
+      <div>
+        {country.map((c) => (
+          <button onClick={e=>checkAnswer(c)}>{c.name}</button>
+        ))}
+      </div>
+      <div>{ showAnswer ? ( <h2 className={flagCountry.name === selected.name ? 'correct':'incorrect' }> Correct : {flagCountry.name}</h2>) : null}</div>
+    </div>
+  );
+};
+
+export default App;
